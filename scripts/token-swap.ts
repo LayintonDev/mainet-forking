f2f220dc6edb0e755545f5e6d8cdda7c265b25fc
LayintonDev <layintondev@gmail.com>
2024-09-13 17:34:39 +0100

    initital commit

--- scripts/token-swap.ts
+++ scripts/token-swap.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 async function main() {
     const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-    const USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+    const USDC = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
     const LSK = "0x6033F7f88332B8db6ad452B7C6D5bB643990aE3f";
     const LIQUIDITY_RECEIVER = "0x9ce826910f5e22A6e22A6a0418033b2677505752";
     const TOKEN_HOLDER = "0x28C6c06298d514Db089934071355E5743bf21d60";
@@ -19,7 +19,7 @@ async function main() {
     const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
 
     const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS);
-    const USDT_Contract = await ethers.getContractAt("IERC20", USDT, impersonatedSigner);
+    const USDT_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
     const LSK_Contract = await ethers.getContractAt("IERC20", LSK, impersonatedSigner);
 
     const TOKEN_HOLDER_USDT_BAL_BEFORE = await USDT_Contract.balanceOf(TOKEN_HOLDER);
@@ -33,7 +33,7 @@ async function main() {
     await USDT_Contract.approve(ROUTER, DESIRED_USD);
     await LSK_Contract.approve(ROUTER, DESIRED_LSK);
 
-    await ROUTER.addLiquidity(USDT, LSK, DESIRED_USD, DESIRED_LSK, MIN_USD, MIN_LSK, LIQUIDITY_RECEIVER, deadline); // ROUTER.addLiquidity
+    await ROUTER.addLiquidity(USDC, LSK, DESIRED_USD, DESIRED_LSK, MIN_USD, MIN_LSK, LIQUIDITY_RECEIVER, deadline); // ROUTER.addLiquidity
 
     const TOKEN_HOLDER_USDT_BAL_AFTER = await USDT_Contract.balanceOf(TOKEN_HOLDER);
     const TOKEN_HOLDER_LSK_BAL_AFTER = await LSK_Contract.balanceOf(TOKEN_HOLDER);
